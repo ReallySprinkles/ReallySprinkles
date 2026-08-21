87739364d759772b2f81fdee97423733bfe6a5d7
sprinkles <ovendalegend@gmail.com>
2026-08-22 02:26:27 +0300

    Update supabase-config.js

--- tunely/js/supabase-config.js
+++ tunely/js/supabase-config.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://ovqtibumfhlifhghqukq.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92cXRpYnVtZmhsaWZoZ2hxdWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczNDUyMDcsImV4cCI6MjEwMjkyMTIwN30.5y2CFugu3vaZB1EVGojF1VUZsXje4DsoGPK5gz4ZSvQ';
+const SUPABASE_URL = 'https://kdezxjizwqfmugkwpycb.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkZXp4aml6d3FmbXVna3dweWNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxNzQ5OTgsImV4cCI6MjEwMjc1MDk5OH0.Pc2p7s3umB-7PyPtTupQcA5occ5xEZdQzs4K5Nwhv_8';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
