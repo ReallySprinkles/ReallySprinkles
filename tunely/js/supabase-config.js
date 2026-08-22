75c45e1e7264b0161e9ff2fc91d1472444925d7f
sprinkles <ovendalegend@gmail.com>
2026-08-22 04:58:14 +0300

    Delete tunely directory

--- tunely/js/supabase-config.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-
-const SUPABASE_URL = 'https://kdezxjizwqfmugkwpycb.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkZXp4aml6d3FmbXVna3dweWNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxNzQ5OTgsImV4cCI6MjEwMjc1MDk5OH0.Pc2p7s3umB-7PyPtTupQcA5occ5xEZdQzs4K5Nwhv_8';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
